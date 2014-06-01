7f5cf148ca4a6a7dfef67dde927bc8844e30f3ca
Jon Hinks <projects.ws@jondh.me.uk>
2014-06-01 17:31:16 +0100

    Set up more test data

--- test/generate-diffs.sh
+++ test/generate-diffs.sh
@@ -1,4 +1,14 @@
+# Generates diff data from repo commits
+
+# Save pwd and then change dir to the script location
+STARTDIR=`pwd`
+cd `dirname $0`
+
 git show 11ecdc1a8ba9a13fdb212d7788af1b79d81e90a7 > diffs/add-lines1
 
 git show 0c8bc141825f8a7b40db6bf121984586386ae7fe > diffs/del-lines1
 
+git show b720096ea7a3ceab3cf566c75678ef079ea758e2 > diffs/add-del1
+
+# Go back to original dir
+cd $STARTDIR 
