c1dcd44789a78a55aa86c368b4411a5d91a7fa13
Jon Hinks <projects.ws@jondh.me.uk>
2014-06-01 18:06:18 +0100

    Add last test, which fails

--- test/generate-diffs.sh
+++ test/generate-diffs.sh
@@ -10,5 +10,7 @@ git show 0c8bc141825f8a7b40db6bf121984586386ae7fe > diffs/del-lines1
 
 git show b720096ea7a3ceab3cf566c75678ef079ea758e2 > diffs/add-del1
 
+git show bb8c8171af04fe0e7bb5cd88ca1927f810511ab8 > diffs/modify-lines1
+
 # Go back to original dir
 cd $STARTDIR 
