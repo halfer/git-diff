97d0c1127cbf4a0f9e2e29d9b7700fcdc51d842c
Jon Hinks <projects.ws@jondh.me.uk>
2014-08-17 17:35:05 +0100

    Tidier version of the above

--- templates/page.php
+++ templates/page.php
@@ -5,14 +5,14 @@
 <div class="file <?php if ($forceStatus) echo 'full-width' ?>">
 	<?php if ($forceStatus != ilovephp\DiffPage::INSERTED): ?>
 		<div class="left side">
-			<div style="display: table;">
+			<div class="inner-side">
 				<?php $page->renderSide('left', (bool) $forceStatus) ?>
 			</div>
 		</div>	
 	<?php endif ?>
 	<?php if ($forceStatus != ilovephp\DiffPage::DELETED): ?>
 		<div class="right side">
-			<div style="display: table;">
+			<div class="inner-side">
 				<?php $page->renderSide('right', (bool) $forceStatus) ?>
 			</div>
 		</div>
