2af79d1b4d56d5b2cef5da5f481e52d6a95f7694
Jon Hinks <projects.ws@jondh.me.uk>
2014-08-17 17:17:56 +0100

    Now CSS table rows sit in the right place

--- templates/page.php
+++ templates/page.php
@@ -5,12 +5,16 @@
 <div class="file <?php if ($forceStatus) echo 'full-width' ?>">
 	<?php if ($forceStatus != ilovephp\DiffPage::INSERTED): ?>
 		<div class="left side">
-			<?php $page->renderSide('left', (bool) $forceStatus) ?>
+			<div style="display: table;">
+				<?php $page->renderSide('left', (bool) $forceStatus) ?>
+			</div>
 		</div>	
 	<?php endif ?>
 	<?php if ($forceStatus != ilovephp\DiffPage::DELETED): ?>
 		<div class="right side">
-			<?php $page->renderSide('right', (bool) $forceStatus) ?>
+			<div style="display: table;">
+				<?php $page->renderSide('right', (bool) $forceStatus) ?>
+			</div>
 		</div>
 	<?php endif ?>
 </div>
