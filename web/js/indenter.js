ec3b78a31aa7f35eb1a726d8cc962882bbc03a28
Jon Hinks <projects.ws@jondh.me.uk>
2014-08-17 22:29:06 +0100

    Refactor indenter as jQuery widget

--- web/js/indenter.js
+++ web/js/indenter.js
@@ -1,28 +1,33 @@
 /*
  * Swaps tab indents with blocks of hard spaces
+ * 
+ * Implemented as a jQuery plugin
  */
 
-$(document).ready(function() {
-	// Set up replacement indent
-	var indentString = '&nbsp;&nbsp;&nbsp;&nbsp;';
+(function($) {
+	$.fn.gitDiffIndentTabs = function(options) {
+		// These options can be overridden if necessary
+		var defaults = {
+			codeCss: '.file .side .line code',
+			indentString: '&nbsp;&nbsp;&nbsp;&nbsp;'
+		};
+		var options = $.extend(defaults, options);
 
-	// Iterate across each code block
-	var codeBlocks = $('.container .file .side');
-	codeBlocks.each(function(ord, blockElement) {
-		// Iterate across each line
-		var lines = $(blockElement).find('.line code');
-		lines.each(function(ord, lineElement) {
-			var lineText = $(lineElement).text();
-			var newText = lineText.replace(/^\t+/, '');
-			if (newText.length < lineText.length)
-			{
-				var addSpaces = lineText.length - newText.length;
-				for(var i = 0; i < addSpaces; i++)
+		return this.each(function() {
+			var lines = $(this).find(options.codeCss);
+			lines.each(function(ord, lineElement) {
+				var lineText = $(lineElement).text();
+				var newText = lineText.replace(/^\t+/, '');
+				if (newText.length < lineText.length)
 				{
-					newText = indentString + newText;
+					var addSpaces = lineText.length - newText.length;
+					for(var i = 0; i < addSpaces; i++)
+					{
+						newText = options.indentString + newText;
+					}
+					$(lineElement).html(newText);
 				}
-				$(lineElement).html(newText);
-			}
+			});
 		});
-	});
-});
+	};
+})(jQuery);
